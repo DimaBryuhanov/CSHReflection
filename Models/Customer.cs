e4b6bf970fe2df28d81a121a46202144bb1f3ffb
dmitrii_bryuhanov@outlook.com <dmitrii_bryuhanov@outlook.com>
2020-07-06 14:50:14 +0300

    Add project files.

---
+++ Models/Customer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSHReflection.Models
+{
+    class Customer
+    {
+
+        public Customer()
+        {
+            //Defaults
+        }
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string Lastname { get; set; }
+
+        public string Address { get; set; }
+
+        public bool Validate(Customer customerObj)
+
+        {
+            //Code to validate the customer object
+            return true;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using CSHReflection.Models;
+using System;
+using System.Reflection;
+
+namespace CSHReflection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type type = typeof(Customer);
+
+            //Getting a class's properties:
+            PropertyInfo[] propertyInfo = type.GetProperties();
+            Console.WriteLine("The list of properties of the Customer class are:--");
+            foreach (PropertyInfo pInfo in propertyInfo)
+            {
+                Console.WriteLine(pInfo.Name);
+            }
+            Console.WriteLine("\n");
+
+            //Getting class's constructors:
+            ConstructorInfo[] constructorInfo = type.GetConstructors();
+            Console.WriteLine("The Customer class contains the following Constructors:--");
+            foreach (ConstructorInfo c in constructorInfo)
+            {
+                Console.WriteLine(c);
+            }
+            Console.WriteLine("\n");
+
+            MethodInfo[] methodInfo = type.GetMethods();
+            Console.WriteLine("The methods of the Customer class are:--");
+            foreach (MethodInfo temp in methodInfo)
+            {
+                Console.WriteLine(temp.Name);
+            }
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Methods' Attributes:");
+            foreach (MethodInfo temp in methodInfo)
+            {
+                foreach (Attribute attribute in temp.GetCustomAttributes(true))
+                {
+                    Console.WriteLine(attribute);
+                }
+            }
+
+
+            Console.Read();
+
+        }
+    }
+}
